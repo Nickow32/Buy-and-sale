779d8b86281dca5c2bd54e81b8f823fd62d40c38
Pavel <vip00z@yandex.ru>
2022-03-31 01:20:10 +0300

    v0.1 Начало

---
+++ data/products.py
@@ -0,0 +1,17 @@
+import sqlalchemy
+from sqlalchemy import orm
+
+from data.db_session import SqlAlchemyBase
+
+
+class Product(SqlAlchemyBase):
+    __tablename__ = 'products'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    price = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
